71fc06ade02c55dbcec2ef3a011a1082390e29a7
xiahao <wangxiahaosin@gmail.com>
2019-06-01 16:06:30 -0700

    adds final data

--- Final Project/smart_beta.R
+++ Final Project/smart_beta.R
@@ -61,7 +61,6 @@ setkey(compustat_PRBA,gvkey,year)
 compustat_merged <- merge(compustat_annual, compustat_PRBA, by.x = c("gvkey","fyear"), by.y = c("gvkey","year"), all.x = T)
 #####################################################################################
 
-
 ########################### Calculate fundamental factors on compustat data ###########################
 # calculates the shareholders equity (SHE)
 compustat_merged[,SHE:= coalesce(seq, ceq + pstk, at - lt - mib, at - lt)]
@@ -163,37 +162,37 @@ setorder(finaldata, date)
 finaldata[, Year:= year(date)]
 finaldata[, Month:= month(date)]
 finaldata <- finaldata[Year < 2018,]
-#####################################################################################
-
-################################ sorting the 1000 largest stocks based on each metrics ####################################
-finaldata[, BE_index := 1]
-finaldata[, CF_NET_index := 1]
-finaldata[, dvt_index := 1]
-finaldata[, revt_index := 1]
-finaldata[, emp_index := 1]
-finaldata[, sale_index := 1]
-
-# sort BE and label it from largest to smallest (1 to 1000)
-setorderv(finaldata, cols = c("Year", "Month", "BE"), order =-1, na.last = T)
-finaldata[, BE_index := cumsum(BE_index), .(Year, Month)]
-
-# sort Cash flow and label it from largest to smallest (1 to 1000)
-setorderv(finaldata, cols = c("Year", "Month", "CF_NET"), order =-1, na.last = T)
-finaldata[, CF_NET_index := cumsum(CF_NET_index), .(Year, Month)]
-
-# sort dividend and label it from largest to smallest 
-setorderv(finaldata, cols = c("Year",  "Month", "dvt"), order = -1, na.last = T)
-finaldata[, dvt_index := cumsum(dvt_index), .(Year, Month)]
-
-# sort revenue and label it from largest to smallest
-setorderv(finaldata, cols = c("Year","Month","revt"), order = -1, na.last = T)
-finaldata[, revt_index := cumsum(revt_index), .(Year, Month)]
-
-# sort employment and label it from largest to smallest 
-setorderv(finaldata, cols = c("Year","Month","emp"), order = -1, na.last = T)
-finaldata[, emp_index := cumsum(emp_index), .(Year, Month)]
-
-# sort sales and label it from largest to smallest
-setorderv(finaldata, cols = c("Year","Month","sale"), order = -1, na.last = T)
-finaldata[, sale_index := cumsum(sale_index), .(Year, Month)]
-#####################################################################################
+# #####################################################################################
+# 
+# ################################ sorting the 1000 largest stocks based on each metrics ####################################
+# finaldata[, BE_index := 1]
+# finaldata[, CF_NET_index := 1]
+# finaldata[, dvt_index := 1]
+# finaldata[, revt_index := 1]
+# finaldata[, emp_index := 1]
+# finaldata[, sale_index := 1]
+# 
+# # sort BE and label it from largest to smallest (1 to 1000)
+# setorderv(finaldata, cols = c("Year", "Month", "BE"), order =-1, na.last = T)
+# finaldata[, BE_index := cumsum(BE_index), .(Year, Month)]
+# 
+# # sort Cash flow and label it from largest to smallest (1 to 1000)
+# setorderv(finaldata, cols = c("Year", "Month", "CF_NET"), order =-1, na.last = T)
+# finaldata[, CF_NET_index := cumsum(CF_NET_index), .(Year, Month)]
+# 
+# # sort dividend and label it from largest to smallest 
+# setorderv(finaldata, cols = c("Year",  "Month", "dvt"), order = -1, na.last = T)
+# finaldata[, dvt_index := cumsum(dvt_index), .(Year, Month)]
+# 
+# # sort revenue and label it from largest to smallest
+# setorderv(finaldata, cols = c("Year","Month","revt"), order = -1, na.last = T)
+# finaldata[, revt_index := cumsum(revt_index), .(Year, Month)]
+# 
+# # sort employment and label it from largest to smallest 
+# setorderv(finaldata, cols = c("Year","Month","emp"), order = -1, na.last = T)
+# finaldata[, emp_index := cumsum(emp_index), .(Year, Month)]
+# 
+# # sort sales and label it from largest to smallest
+# setorderv(finaldata, cols = c("Year","Month","sale"), order = -1, na.last = T)
+# finaldata[, sale_index := cumsum(sale_index), .(Year, Month)]
+# #####################################################################################
